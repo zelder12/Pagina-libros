73888f863a0255214e6d51ba86f01862c3e5a707
Roberto Carlos <131389535+zelder12@users.noreply.github.com>
2025-03-21 09:10:31 -0600

    Update preScript.js

--- js/preScript.js
+++ js/preScript.js
@@ -5,7 +5,7 @@ if (window.location.pathname.includes('presentacion1.html')) {
 } else if (window.location.pathname.includes('presentacion2.html')) {
     url = '../presentacion-txt/pre_libro2.pdf';
 } else if (window.location.pathname.includes('presentacion3.html')) {
-    url = '../presentacion-txt/pre-libro3.pdf';
+    url = '../presentacion-txt/pre_libro3.pdf';
 } else {
     console.error('No se encontró un presentacion para esta página.');
 }
